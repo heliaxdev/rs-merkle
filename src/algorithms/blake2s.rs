aa4cc9fb8ad2c678b90d864a5e7e9acb24c5977c
Simon Masson <simon.masson@yahoo.fr>
2022-02-07 09:34:34 -0400

    blake2s file

---
+++ src/algorithms/blake2s.rs
@@ -0,0 +1,16 @@
+use crate::Hasher;
+use blake2::{digest::FixedOutput, Digest, Blake2s256};
+
+#[derive(Clone)]
+pub struct Blake2sAlgorithm {}
+
+impl Hasher for Blake2sAlgorithm {
+    type Hash = [u8; 32];
+
+    fn hash(data: &[u8]) -> [u8; 32] {
+        let mut hasher = Blake2s256::new();
+
+        hasher.update(data);
+        <[u8; 32]>::from(hasher.finalize_fixed())
+    }
+}
